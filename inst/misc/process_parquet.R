367c9673826e2031fe29c4ca43c3aef98d4e0141
Markus Kainu <muuankarski@kapsi.fi>
2025-04-16 14:06:54 +0300

    kirjoitetaan myös listaus kaikista datoista

--- inst/misc/process_parquet.R
+++ inst/misc/process_parquet.R
@@ -20,6 +20,22 @@ for (i in seq(n_datasets)){
   download.file(json_link, destfile = paste0("./inst/data-parquet/",id,".json"))
 }
 
+dlist <- list()
+for (i in seq(n_datasets)){
+  print(i)
+  tmplist <- list()
+  tmplist$id <- datasets$datasets[[i]]
+  pkg_lst <- pick_a_pkg(input_dataset = id)
+  tmplist$name_clean <- pkg_lst$pkg$name
+  tmplist$title <- pkg_lst$pkg$title
+  tmplist$url_tietotarjotin <- pkg_lst$pkg$external_urls[[1]]
+  tmplist$updated <- pkg_lst$pkg$resources[[2]]$status_updated
+  tmplist$csv_links <- get_csv_links(pkg_lst = pkg_lst)
+  tmplist$json_link <- pkg_lst$pkg$resources[[pkg_lst$json_nr]]$url
+  dlist[[i]] <- tmplist
+}
+jsonlite::write_json(x = dlist, path = "./inst/data-parquet/datasets.json")
+
 system(paste0("rsync -avz ./inst/data-parquet/ ",Sys.getenv("kelaopendata_server")))
 
 
