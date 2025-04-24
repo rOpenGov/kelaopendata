2361a793d37d80772bb1af8613b28ffc4fc707da
Markus Kainu <muuankarski@kapsi.fi>
2025-04-24 17:58:45 +0300

    datasetit kahteen eri paikkaan, että outo cache-ongelma kelassa ratkeaisi

--- inst/misc/process_parquet.R
+++ inst/misc/process_parquet.R
@@ -24,17 +24,22 @@ dlist <- list()
 for (i in seq(n_datasets)){
   print(i)
   tmplist <- list()
-  tmplist$id <- datasets$datasets[[i]]
+  id <- datasets$datasets[[i]]
+  tmplist$id <- id
   pkg_lst <- pick_a_pkg(input_dataset = id)
+  if (length(pkg_lst$pkg$resources) == 0) next()
   tmplist$name_clean <- pkg_lst$pkg$name
   tmplist$title <- pkg_lst$pkg$title
   tmplist$url_tietotarjotin <- pkg_lst$pkg$external_urls[[1]]
   tmplist$updated <- pkg_lst$pkg$resources[[2]]$status_updated
   tmplist$csv_links <- get_csv_links(pkg_lst = pkg_lst)
   tmplist$json_link <- pkg_lst$pkg$resources[[pkg_lst$json_nr]]$url
+  dta <- arrow::read_parquet(paste0("./inst/data-parquet/",id,".parquet"))
+  tmplist$dhead <- head(dta)
   dlist[[i]] <- tmplist
 }
-jsonlite::write_json(x = dlist, path = "./inst/data-parquet/datasets.json")
+jsonlite::write_json(x = dlist, path = "./inst/data-parquet/datasets.json", pretty = TRUE)
+jsonlite::write_json(x = dlist, path = "./inst/data-parquet/json/datasets.json", pretty = TRUE)
 
 system(paste0("rsync -avz ./inst/data-parquet/ ",Sys.getenv("kelaopendata_server")))
 
